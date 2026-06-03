ed8465a5daa7a2b82881b6434f6cf8b6285ab8b1
hilarioluigi <hilarioluigi3@gmail.com>
2026-06-03 19:44:09 +0700

    fix: increase timeout

--- src/main/java/actionDriver/WaitAction.java
+++ src/main/java/actionDriver/WaitAction.java
@@ -12,12 +12,10 @@ import java.time.Duration;
 public class WaitAction {
     WebDriver driver;
     WebDriverWait wait;
-    WebElement element;
 
     public WaitAction(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        //this.element = element;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
     public void click(By locator) {
